1e72000c1f69e828f5479b4ee4764c72172b804a
naama-knoll <naama2745@gmail.com>
2021-12-26 14:43:57 +0200

    add project

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique deleniti natus, consequatur explicabo in quo totam cumque sit accusamus doloribus iste fugiat repellat accusantium officia a illo amet nulla modi beatae dignissimos? Ipsam voluptate expedita quod modi minus molestias ipsum!</p>
+    </section>
+  )
+}
+
+export default About
